790d37b71b26c3d637c9215fde234dca2512f977
amykangweb <selene6023@gmail.com>
2016-02-20 11:21:29 -0800

    initial commit.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Three ways to return reversed string.
+
+            // Print out string in reverse order.
+            string HelloWorld = "Hello World!";
+
+            for(var i = HelloWorld.Length - 1; i >= 0; i--)
+            {
+                Console.Write(HelloWorld[i]);
+            }
+
+            // Save reversed string to variable and print out.
+            string ReversedString = "";
+
+            for(var i = HelloWorld.Length - 1; i >= 0; i--)
+            {
+                ReversedString = ReversedString + HelloWorld[i];
+            }
+            Console.WriteLine(ReversedString);
+
+            // Convert string to array then reverse and print out.
+            char[] ReversedArray = HelloWorld.ToCharArray();
+            Array.Reverse(ReversedArray);
+            Console.WriteLine(string.Join("", ReversedArray));
+            Console.Read();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+<h1>Reverse A String In C#</h1>
+
+<h2>Information</h2>
+
+By Amy Kang
+
+<h2>Description</h2>
+
+Three ways to return a reversed string in C#. Print out each letter, save each letter to a variable, convert string to array then reverse and join.
